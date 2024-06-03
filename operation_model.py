5dd613dc18708c69f424038d6f3dd59405ca8ddf
liyb2002 <williamliyuanbo@gmail.com>
2024-06-03 15:36:04 +0800

    can load data

--- operation_model.py
+++ operation_model.py
@@ -2,6 +2,8 @@ import Preprocessing.dataloader
 
 from torch.utils.data import DataLoader
 from tqdm import tqdm 
+from config import device
+import torch
 
 
 dataset = Preprocessing.dataloader.Program_Graph_Dataset()
@@ -11,4 +13,11 @@ data_loader = DataLoader(dataset, batch_size=1, shuffle=True)
 for batch in tqdm(data_loader):
     node_features, operations_matrix, intersection_matrix, program, face_embeddings, edge_embeddings, vertex_embeddings = batch
 
-    print("node_features", node_features.shape)
+    node_features = node_features.to(torch.float32).to(device)
+    operations_matrix = operations_matrix.to(torch.float32).to(device)
+    intersection_matrix = intersection_matrix.to(torch.float32).to(device)
+
+    face_embeddings = face_embeddings.to(torch.float32).to(device)
+    edge_embeddings = edge_embeddings.to(torch.float32).to(device)
+    vertex_embeddings = vertex_embeddings.to(torch.float32).to(device)
+
