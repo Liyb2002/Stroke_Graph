bdcd1624f1782c57162255f625f06f01f4b5b275
liyb2002 <williamliyuanbo@gmail.com>
2024-06-11 11:07:29 +0800

    vis ground truth

--- sketch_operation_prediction.py
+++ sketch_operation_prediction.py
@@ -18,6 +18,9 @@ import torch.optim as optim
 import os
 import pandas as pd
 
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 # Define the neural networks
 graph_embedding_model = Encoders.gnn.gnn.SemanticModule()
 SBGCN_model = Preprocessing.SBGCN.SBGCN_network.FaceEdgeVertexGCN()
@@ -59,6 +62,30 @@ def get_kth_operation(op_to_index_matrix, k):
 
     return kth_operation
 
+
+def plot_strokes_3d(strokes, stroke_features):
+    # Ensure strokes and stroke_features are tensors
+    strokes = strokes.clone().detach()
+    stroke_features = stroke_features.clone().detach().squeeze(0)
+    
+    num_chosen_strokes = torch.sum(strokes).item()
+    print(f"Number of chosen strokes in the strokes matrix: {num_chosen_strokes}")
+
+    # Create a 3D plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    
+    num_strokes = stroke_features.shape[0]
+    
+    for i in range(num_strokes):
+        x = [stroke_features[i, 0].item(), stroke_features[i, 3].item()]
+        y = [stroke_features[i, 1].item(), stroke_features[i, 4].item()]
+        z = [stroke_features[i, 2].item(), stroke_features[i, 5].item()]
+        
+        color = 'red' if strokes[i, 0].item() == 1 else 'blue'
+        ax.plot(x, y, z, color=color)
+    
+    plt.show()
 
 def train():
 
@@ -192,10 +219,78 @@ def train():
             save_models()
 
 
+def eval(vis=True):
+    load_models()
+
+    graph_embedding_model.eval()
+    SBGCN_model.eval()
+    sketch_attention_model.eval()
+
+    # Define evaluation criterion
+    criterion = nn.BCEWithLogitsLoss()
+
+    # Create DataLoader
+    dataset = Preprocessing.dataloader.Program_Graph_Dataset('dataset/eval_dataset')
+
+    # Filter to only keep good data
+    good_data_indices = [i for i, data in enumerate(dataset) if data[4][-1] == 1]
+    filtered_dataset = Subset(dataset, good_data_indices)
+    print(f"Total number of sketch data: {len(filtered_dataset)}")
+
+    eval_loader = DataLoader(filtered_dataset, batch_size=1, shuffle=False)
+
+    total_eval_loss = 0.0
+
+    with torch.no_grad():
+        for batch in tqdm(eval_loader, desc="Evaluating"):
+            node_features, operations_matrix, intersection_matrix, operations_order_matrix, program, face_features, edge_features, vertex_features, edge_index_face_edge_list, edge_index_edge_vertex_list, edge_index_face_face_list, index_id = batch
+
+            # to device 
+            node_features = node_features.to(torch.float32).to(device)
+            operations_matrix = operations_matrix.to(torch.float32).to(device)
+            intersection_matrix = intersection_matrix.to(torch.float32).to(device)
+            operations_order_matrix = operations_order_matrix.to(torch.float32).to(device)
+
+            gnn_graph = Preprocessing.gnn_graph.SketchHeteroData(node_features, operations_matrix, intersection_matrix, operations_order_matrix)
+            gnn_graph.to_device(device)
+            graph_embedding = graph_embedding_model(gnn_graph.x_dict, gnn_graph.edge_index_dict)
+
+            if face_features.shape[1] == 0:
+                # is empty program
+                brep_embedding = torch.zeros(1, 1, 32, device=device)
+            else:
+                brep_graph = Preprocessing.SBGCN.SBGCN_graph.GraphHeteroData(face_features, edge_features, vertex_features, 
+                            edge_index_face_edge_list, edge_index_edge_vertex_list, edge_index_face_face_list, index_id)
+                
+                brep_graph.to_device(device)
+                face_embedding, edge_embedding, vertex_embedding = SBGCN_model(brep_graph)
+                brep_embedding = torch.cat((face_embedding, edge_embedding, vertex_embedding), dim=1)
+
+            output = sketch_attention_model(graph_embedding, brep_embedding)
+
+            # prepare ground_truth
+            target_op_index = len(program[0])-1
+            op_to_index_matrix = gnn_graph['stroke'].z
+
+            gt_matrix = get_kth_operation(op_to_index_matrix, target_op_index).to(device)
+
+            output = output.view(-1, 1).to(torch.float32)
+            gt_matrix = gt_matrix.view(-1, 1).to(torch.float32)
+
+            loss = criterion(output, gt_matrix)
+            total_eval_loss += loss.item()
+
+            if vis:
+                plot_strokes_3d(gt_matrix, gnn_graph['stroke'].x)
+                break
+
+    avg_eval_loss = total_eval_loss / len(eval_loader)
+    print(f"Evaluation Loss: {avg_eval_loss}")
+
 
 
 
 
 #---------------------------------- Public Functions ----------------------------------#
 
-train()
+eval()
